7e11eb9b537a79eb219c77847a784277738bd8a0
masaya tsukada <mmdunity9856@gmail.com>
2025-12-26 18:29:34 +0900

    インデントの調整

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ $axios('https://pokeapi.co/api/v2/pokemon/?limit=151')
     .then(response => {
       const imgPath = response.data.sprites.other['official-artwork'].front_default;
       $axios(response.data.species.url)
-      .then(response => {
-        const characterName = response.data.names[0].name;
-        const imgElement = `<li><div class="character"><img src="${imgPath}" width="475" height="475" alt="" class="character__img"></div>`;
-        const nameElement = `<p class="character__name">${characterName}</p>`;
-        const fragment = createElements(imgElement + nameElement);
-        characterElement.appendChild(fragment);
-        characterElement.classList.add('list');
-        console.log(response);
-      })
-      .catch(error => {
+    .then(response => {
+      const characterName = response.data.names[0].name;
+      const imgElement = `<li><div class="character"><img src="${imgPath}" width="475" height="475" alt="" class="character__img"></div>`;
+      const nameElement = `<p class="character__name">${characterName}</p>`;
+      const fragment = createElements(imgElement + nameElement);
+      characterElement.appendChild(fragment);
+      characterElement.classList.add('list');
+      console.log(response);
+    })
+    .catch(error => {
         characterElement.after(createErrorElement(error));
         });
     }))
@@ -30,7 +30,7 @@ $axios('https://pokeapi.co/api/v2/pokemon/?limit=151')
         characterElement.after(createErrorElement(error));
         });
   })
-  .catch(error => {
+    .catch(error => {
         console.log(error)
         characterElement.after(createErrorElement(error));
         });
